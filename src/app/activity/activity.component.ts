4d14ab8cc47857b86735c76e42dc40786cc77f30
DDosSantos <denis.sws@gmail.com>
2017-12-15 09:22:19 +0100

    ajout et modif d'une activite ok sans la base de données

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params, NavigationExtras } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Activity } from '../activity';
+import { Category } from '../category';
 
 @Component({
   selector: 'app-activity',
@@ -7,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
-  constructor() { }
+  private subscriptionParam: Subscription;
+  public activity: Activity;
+  public categories: Category[];
+  public colors: string[];
 
-  ngOnInit() {
+  public toppings: FormControl;
+
+  constructor(private route: ActivatedRoute) {
+    this.activity = new Activity();
+    this.categories = CATEGORIES;
+    this.colors = COLORS;
+    this.toppings = new FormControl();
   }
 
-  categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6'];
+  ngOnInit() {
+    this.subscriptionParam = this.route.params.subscribe((param: any) => {
+      // Search the activity by name
+        if((param['id'])!=null){ //Recuperer l'activité avec param['id'] quand la base de données sera ok
+          this.activity.name = "Activity 0";
+          this.activity.description = "description";
+          this.activity.color = "green";
+          this.activity.addCategory(CATEGORIES[0]);
+        }
+    });
+  }
+
+  // afficherActivity(){
+  //   console.log(this.activity);
+  // }
+  //
+  // compareCat(){
+  //   console.log(this.activity.getCategories());
+  // }
 }
+
+// Temporary elements for debug purposes until dataService is available
+const CATEGORIES: Category[] = [
+  { "name": "Code", "icon": "code" },
+  { "name": "Sleep", "icon": "airline_seat_individual_suite" },
+  { "name": "Call", "icon": "call" },
+  { "name": "Games", "icon": "casino" },
+  { "name": "Movies", "icon": "movies" }
+]
+
+const COLORS: string[] = [
+  "red", "green", "blue"
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
@@ -41,7 +41,8 @@ const routes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(routes),
-    AngularMaterialModule
+    AngularMaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
