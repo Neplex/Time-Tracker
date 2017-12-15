13c35209808c6b5b7461906e975e04e46f62678f
GRETTE <yanael.grette@outlook.com>
2017-12-15 15:38:55 +0100

    Display activities in stats page as a table. Need to calculate elapsed time for each activity (not done yet)

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Activity } from '../activity';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { Category } from '../category';
+import { TimeSlot } from '../time-slot';
 
 @Component({
   selector: 'app-stats',
@@ -7,10 +10,106 @@ import { Activity } from '../activity';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
+  /*Prepare table*/
+  displayedCols = ["name","description","elapse"];
+  CATEGORIES: Category[] = [];
+  ACTIVITIES: Activity[] = [];
+  dataSource:MatTableDataSource<Activity> = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   constructor() { }
 
   ngOnInit() {
+
+    /**********tests**********/
+
+
+    //creating activities
+
+    let c = new Category();
+    c.name = "development"
+    this.CATEGORIES.push(c);
+
+    let a = new Activity();
+    a.name = "Web project";
+    a.color = "blue";
+    a.addCategory (c);
+    a.description = "Working on web project"
+    /*a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-23T10:20:00"),
+      new Date("2017-11-23T11:34:00")
+    ));
+
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-24T10:20:00"),
+      new Date("2017-11-24T11:34:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-26T10:20:00"),
+      new Date("2017-11-26T11:34:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-12-08T10:00:00"),
+      new Date("2017-12-08T11:00:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-12-08T10:30:00"),
+      new Date("2017-12-08T11:30:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-12-08T15:30:00"),
+      new Date("2017-12-08T17:30:00")
+    ));*/
+    this.ACTIVITIES.push(a);
+    c = new Category();
+    c.name = "relax";
+    a = new Activity();
+    a.name = "Have fun";
+    a.color = "teal";
+    a.addCategory(c);
+    a.description = "Relaxing with good vibes";
+    this.CATEGORIES.push(c);
+    /*
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-23T12:00:00"),
+      new Date("2017-11-23T14:00:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-23T16:00:00"),
+      new Date("2017-11-23T17:00:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-11-24T23:00:00"),
+      new Date("2017-11-25T01:00:00")
+    ));
+    a.addTimeSlot(new TimeSlot(
+      new Date("2017-12-08T08:00:00"),
+      new Date("2017-12-08T09:00:00")
+    ));*/
+    this.ACTIVITIES.push(a);
+    c = new Category();
+    c.name = "sport";
+    a = new Activity();
+    a.name = "badminton";
+    a.color = "blue";
+    a.addCategory(c);
+    a.description = "Playing with main gauche";
+    this.CATEGORIES.push(c);
+    this.ACTIVITIES.push(a);
+    console.log("HEY");
+    for( let i = 0; i < 25; ++i) {
+      this.ACTIVITIES.push(a);
+    }
+    this.dataSource = new MatTableDataSource<Activity>(this.ACTIVITIES);
+
+    //
+  }
+
+  calculTemps(activity:Activity): string {
+    return "Je dois calculer le time ICI ";
   }
 
 }
