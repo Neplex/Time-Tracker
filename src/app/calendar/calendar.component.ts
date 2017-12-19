95d46d26fa8651696af483dd7630e2b0ebc9bfeb
Nicolas HIOT <nicolas.hiot@etu.univ-orleans.fr>
2017-12-19 16:37:17 +0100

    Connect calendar to database

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { Subscription } from 'rxjs';
+import { DataStorageService } from '../data-storage/data-storage.service';
 import { Activity } from '../activity';
 import { TimeSlot } from '../time-slot';
 import { CalendarEvent } from '../angular-material-calendar/calendar-event';
@@ -13,72 +15,24 @@ export class CalendarComponent implements OnInit {
 
   public date: Date = new Date();
   public events: CalendarEvent[] = [];
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private dataBase: DataStorageService) { }
 
   ngOnInit() {
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Section for debug purpose
-    const ACTIVITIES: Activity[] = [];
-    let a = new Activity();
-    a.name = "Web project";
-    a.color = "blue";
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-23T10:20:00"),
-      new Date("2017-11-23T11:34:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-24T10:20:00"),
-      new Date("2017-11-24T11:34:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-26T10:20:00"),
-      new Date("2017-11-26T11:34:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-12-08T10:00:00"),
-      new Date("2017-12-08T11:00:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-12-08T10:30:00"),
-      new Date("2017-12-08T11:30:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-12-08T15:30:00"),
-      new Date("2017-12-08T17:30:00")
-    ));
-    ACTIVITIES.push(a);
-    a = new Activity();
-    a.name = "Have fun";
-    a.color = "teal";
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-23T12:00:00"),
-      new Date("2017-11-23T14:00:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-23T16:00:00"),
-      new Date("2017-11-23T17:00:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-11-24T23:00:00"),
-      new Date("2017-11-25T01:00:00")
-    ));
-    a.addTimeSlot(new TimeSlot(
-      new Date("2017-12-08T08:00:00"),
-      new Date("2017-12-08T09:00:00")
-    ));
-    ACTIVITIES.push(a);
-    ////////////////////////////////////////////////////////////////////////////
-
-
-    let activites: Activity[] = ACTIVITIES;
-    activites.forEach(a => {
-      a.getTimeSlots().forEach(ts => {
-        let event = new CalendarEvent(a.name, ts.start, ts.end, a.color);
-        this.events.push(event);
+    this.subscription = this.dataBase.getActivities().subscribe(acts => {
+      acts.forEach(a => {
+        a.getTimeSlots().forEach(ts => {
+          let event = new CalendarEvent(a.name, ts.start, ts.end, a.color);
+          this.events.push(event);
+        });
       });
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
